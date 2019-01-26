6576f0f6a01d528d174d608d1d34425c5b96507f
Nikolai Shabalin <nikolay.shabalin@kelnik.ru>
2019-01-26 17:34:03 +0300

    Переносит файл настроек в диреторию Попапа. Рефаторит Settings

--- src/Popup/Settings/index.js
+++ src/Popup/Settings/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import css from './Settings.module.css';
-import dictionary from '../dictionary';
-import contentCSS from "../Popup/Content/content.module.css";
-import Select from '../Popup/Inputs/Select';
+import dictionary from '../../dictionary';
+import contentCSS from "../../Popup/Content/content.module.css";
+import Select from '../../Popup/Inputs/Select';
 
 export default class Settings extends React.Component {
   render() {
@@ -11,7 +11,7 @@ export default class Settings extends React.Component {
     return (
       <div className={ css.Settings }>
         <label htmlFor="language" className={ contentCSS.label }>{ dictionary[language].changeLanguage }</label>
-        <Select id={ "language" } onChange={ onChangeLanguage } options={ dictionary }/>
+        <Select id={ "language" } onChange={ onChangeLanguage } options={ dictionary } />
       </div>
     )
   }
