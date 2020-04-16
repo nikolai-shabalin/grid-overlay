6d7c8a69390a9acaa02d4ab6d715521723f7f214
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2020-04-16 22:00:42 +0300

    Удаляет всё из проекта

--- src/background.js
+++
@@ -1,3 +0,0 @@
-window.chrome.browserAction.onClicked.addListener(function() {
-  window.chrome.tabs.executeScript(null, {file: "./static/js/content.js"});
-});
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const hexToRgb = hex => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
-};
-
-export const isGutterShow = gutterWidth => {
-  return parseInt(gutterWidth, 10) !== 0;
-};
